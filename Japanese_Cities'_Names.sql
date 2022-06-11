10a22fc41995ff670ebbae227b8f68b815eed5f8
jy0605 <31639823+jy0605@users.noreply.github.com>
2022-06-11 23:34:46 +0900

    Create Japanese_Cities'_Names.sql
    -- basic select

---
+++ Japanese_Cities'_Names.sql
@@ -0,0 +1,7 @@
+-- HackerRank SQL
+-- https://www.hackerrank.com/challenges/japanese-cities-name/problem?isFullScreen=true
+-- basic select
+-- Japanese Cities' Name
+select name
+from CITY
+where COUNTRYCODE='JPN';
