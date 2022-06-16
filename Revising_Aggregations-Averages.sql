fc17abb9c8dc7e73695cdec4838d22ee4438347a
jy0605 <31639823+jy0605@users.noreply.github.com>
2022-06-16 23:18:24 +0900

    ** 2022.6.16
    -- HackerRank
    -- SQL(Medium)

---
+++ Revising_Aggregations-Averages.sql
@@ -0,0 +1,7 @@
+-- HackerRank
+-- https://www.hackerrank.com/challenges/revising-aggregations-the-average-function/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
+-- Revising Aggregations-Average
+-- avg function
+select avg(population)
+from city
+where district='California';
---
+++ Weather_Observation_Station_20.sql
@@ -0,0 +1,6 @@
+-- HackerRank
+-- https://www.hackerrank.com/challenges/weather-observation-station-20/problem?isFullScreen=true
+-- Weather Observation Station 20
+-- median, round function
+select round(median(lat_n),4)
+from station;
